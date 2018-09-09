731bd9a7d131cee8eed667762dda62eaf10c1475
caveinn <caveinncicad@gmail.com>
2018-09-09 13:51:14 +0300

    add get entries functionality

--- static/home.js
+++ static/home.js
@@ -1,14 +1,15 @@
-let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZ…M4N30.y2Qk_Zdw7FIBzbRD5Y2sIzqxh7d0beE6nLJYbJuEPyc'
+let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im5ldyIsImV4cCI6MTUzNjUwNDA1MX0.9wvmqnqHIZpTdCo5UTxh273vFgAbZGxTBp0x4N_2o38"
+
 
 const uri = "https://kevin-diary-v2.herokuapp.com/api/v2/entries"
 let header = new Headers();
-header.append  ("token",token)
+header.append("access_token", token);
 let req = new Request(uri,{
     headers: header,
     method : "GET"
 });
 fetch(req).then((response) => {
-    if(rsponse.ok){
+    if(response.ok){
         console.log(response);
     } }).then((jsondata) => {
 
